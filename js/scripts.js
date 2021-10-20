487ed828833b1c099cecbe1f3d050a28ed266c98
jonathan <davidrodriguezcarvajal686@gmail.com>
2021-10-20 10:14:46 -0500

    cambio el footer y el favicon que daba error

--- js/scripts.js
+++ js/scripts.js
@@ -44,3 +44,14 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+
+const $form = document.querySelector('#form')
+
+$form.addEventListener('submit', handleSubmit)
+
+function handleSubmit(event){
+    event.preventDefault()
+    const form = new FormData(this)
+    console.log(form.get('name'))
+}
